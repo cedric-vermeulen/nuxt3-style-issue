64cedcf7279292102c41d5165c860382f4f3be6c
Cédric Vermeulen <cve@greyfox.be>
2025-03-27 22:53:16 +0100

    setup

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+   compatibilityDate: "2024-11-01",
+   devtools: { enabled: true },
+
+   // This does fix it but not willing to do this for production, not sure what it really does under the hood
+   // future:{compatibilityVersion:4}
+
+   // This does not fix it
+   // features:{inlineStyles: false},
+});
