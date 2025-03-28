88920355b2f84892a79b641af37b1d84370428c6
Cédric Vermeulen <cve@greyfox.be>
2025-03-28 14:32:14 +0100

    add tailwind

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+   <UApp>
+      <img src="~/assets/icons/logo.png" alt="Discover Nuxt" class="h-12 w-auto" />
+      <NuxtRouteAnnouncer />
+      <NuxtWelcome />
+   </UApp>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,8 @@
 export default defineNuxtConfig({
    compatibilityDate: "2024-11-01",
    devtools: { enabled: true },
-
+   modules: ["@nuxt/ui"],
+   css: ["~/assets/css/main.css"],
    // This does fix it but not willing to do this for production, not sure what it really does under the hood
    // future:{compatibilityVersion:4}
 
